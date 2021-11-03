f5b3de03dfb2e09f3ae79e4353d8aedc074e5a79
Miguel Largo <largoamado@gmail.com>
2021-11-02 21:30:03 -0500

    Version hasta donde se realiza el merge

--- imbd/__init__.py
+++ imbd/__init__.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 #Lectira del dataset
-movies = pd.read_csv(r'C:\Users\largo\Documents\JDC\2021-2\DATA SIENCE\ProyectoDataSciencie\imbd\peliculas.csv')
+movies = pd.read_csv(r'C:\Users\largo\Documents\JDC\2021-2\DATA SIENCE\ProyectoDataSciencie\imbd\peliculas.csv', encoding='utf-8')
 
 #separamos los datos
 num =(movies.dtypes == float) | (movies.dtypes == int)
@@ -19,19 +19,23 @@ obj_cols = [c for c in obj.index if obj[c]]
 
 #Histograma de la columna de duraccion
 movies_nums['duration'].hist()
-plt.legend()
 plt.figure()
-plt.show()
 
 #Histograma de la columna de calidad segun IMDB
 movies_nums['imdb_score'].hist()
-plt.legend()
 plt.figure()
-plt.show()
 
-#Histograma de la columna de presupuestos de las peliculas
+#Histograma de la columna de presupuestos de las peliculas con error del presupuesto
 movies_nums['budget'].hist()
-plt.legend()
 plt.figure()
-plt.show()
 
+#Leectura del segundo DataFrame
+financials = pd.read_csv(r'C:\Users\largo\Documents\JDC\2021-2\DATA SIENCE\ProyectoDataSciencie\imbd\thenumbers.csv', encoding='utf-8')
+
+#Realizamos un merge
+financials = financials[['movie_title', 'production_budget', 'worldwide_gross']]
+movies_nums = pd.concat([movies_nums,movies['movie_title']], axis=1)
+movies_v2 = pd.merge(financials, movies_nums, on='movie_title', how='left')
+
+
+plt.show()
